813ad3da7b66f25a5c3e620ee8622fad00c095fb
Nikita Poletaev <97903488+SweeXXX@users.noreply.github.com>
2022-12-02 17:16:51 +0300

    Add files via upload

---
+++ HomeWork11/Classes/ActiveStudent.cs
@@ -0,0 +1,15 @@
+﻿using HomeWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    internal class ActiveStudent:Student
+    {
+        public ActiveStudent(string name, string numberOfGroup) : base(name, numberOfGroup) { PorA = "A"; }
+        public ActiveStudent(string name, int lastMer, string numberOfGroup) : base(name, lastMer, numberOfGroup) { }
+    }
+}
---
+++ HomeWork11/Classes/ConcreateCreators.cs
@@ -0,0 +1,29 @@
+﻿using HomeWork11.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    internal class CreatePassive:Creator
+    {
+        public CreatePassive() : base() { }
+        public CreatePassive(string name) : base(name) { }  
+        public override PassiveStudent CreateStudent(string name, string numberOfGroup)
+        {
+            return new PassiveStudent(name, numberOfGroup);
+        }
+    }
+    internal class CreateActive : Creator
+    {
+        public CreateActive() : base() { }
+        public CreateActive(string name) : base(name) { }
+        public override ActiveStudent CreateStudent(string name, string numberOfGroup)
+        {
+            return new ActiveStudent(name, numberOfGroup);
+        }
+    }
+}
