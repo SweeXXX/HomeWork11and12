1a5e7c4f1487bda0ba3cf632c65a7bc934e2ab96
Nikita Poletaev <97903488+SweeXXX@users.noreply.github.com>
2022-12-02 21:01:16 +0300

    Add files via upload

---
+++ ClassWork/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "-----\n-   -\n-   -\n-----";
+            string str1 = "123\n123\n123";
+            //str = str.Replace("-", "*");
+            //Figure f = new Figure(str);
+            //Figure f2 = new Figure(str1);
+            //f2.PrintNew(false,10);
+            //f.PrintStart();
+
+            //Console.ReadKey();
+            //f.PrintNew(false, 18, 10);
+            //f2.PrintNew(false, 12,15);
+            //Console.ReadKey();
+            Point r = new Point('*');
+            r.PrintNew(10,10, ConsoleColor.Magenta, ConsoleColor.Yellow, true);
+            Console.ReadKey();
+            r.PrintNew(10, 10, ConsoleColor.Yellow, ConsoleColor.Yellow, true);
+
+
+            Console.ReadKey();
+            //Console.WriteLine(Rectangle.FindS(10,10));   
+
+        }
+    }
+}
