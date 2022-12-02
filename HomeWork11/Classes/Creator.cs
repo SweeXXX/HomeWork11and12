ceeee1091bab794554e7a1d23f3979579e14a3a8
Nikita Poletaev <97903488+SweeXXX@users.noreply.github.com>
2022-12-02 14:17:44 +0300

    Add files via upload

---
+++ HomeWork11/Classes/Creator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    internal abstract class Creator
+    {
+        protected string name;
+        protected Creator(string name)
+        {
+            this.name = name;
+        }
+        protected Creator()
+        { }
+        public abstract Student CreateStudent(string name, string numberOfGroup);
+        
+    }
+}
