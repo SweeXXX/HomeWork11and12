33e03c83403dd42e09ebdba6047b57c8a7b95bcb
Nikita Poletaev <97903488+SweeXXX@users.noreply.github.com>
2022-12-02 17:22:05 +0300

    Add files via upload
    работай уже

---
+++ HomeWork11/HomeWork11/Classes/PassiveStudent.cs
@@ -0,0 +1,17 @@
+﻿using HomeWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork11.Classes
+{
+    internal class PassiveStudent:Student
+    {
+        public PassiveStudent(string name, string numberOfGroup) : base(name, numberOfGroup) { PorA = "P"; }
+        protected PassiveStudent(string name, int lastMer, string numberOfGroup) : base(name, lastMer, numberOfGroup) { }
+
+
+    }
+}
---
+++ HomeWork11/HomeWork11/Classes/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    internal abstract class Student
+    {
+        protected string name;
+        protected int lastMer;
+        protected string numberOfGroup;
+        protected string PorA;
+        public string GetNumberOfGroup
+        {
+            get { return numberOfGroup; }
+        }
+        public string GetPorA
+        {
+            get { return PorA; }
+        }
+        public string GetTitle
+        {
+            get { return $"{name}, {numberOfGroup}, {PorA}"; }
+        }
+        protected Student(string name, int lastMer, string numberOfGroup)
+        {
+            this.name = name;
+            this.lastMer = lastMer;
+            this.numberOfGroup = numberOfGroup;
+        }
+        protected Student(string name, string numberOfGroup)
+        {
+            this.name = name;
+            this.numberOfGroup = numberOfGroup;
+        }
+    }
+}
